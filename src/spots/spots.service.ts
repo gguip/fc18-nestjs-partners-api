0a607613a36139bb35c77f55da75258281365dc1
Guilherme Passarinho <guilhermepassarinho@gmail.com>
2024-08-06 01:36:19 -0300

    feat: created ticket and history model

--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -2,28 +2,66 @@ import { Injectable } from '@nestjs/common';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SpotStatus } from '@prisma/client';
+
+type CreateSpotInput = CreateSpotDto & { eventId: string };
 
 @Injectable()
 export class SpotsService {
   constructor(private prismaService: PrismaService) {}
 
-  create(createSpotDto: CreateSpotDto) {
-    return 'This action adds a new spot';
+  async create(createSpotDto: CreateSpotInput) {
+    const event = await this.prismaService.event.findFirst({
+      where: {
+        id: createSpotDto.eventId,
+      },
+    });
+
+    if (!event) {
+      throw new Error('Event not found');
+    }
+
+    return this.prismaService.spot.create({
+      data: {
+        ...createSpotDto,
+        status: SpotStatus.available,
+      },
+    });
   }
 
   findAll(eventId: string) {
-    return `This action returns all spots`;
+    return this.prismaService.spot.findMany({
+      where: {
+        eventId,
+      },
+    });
   }
 
   findOne(eventId: string, spotId: string) {
-    return `This action returns a #${spotId} spot`;
+    return this.prismaService.spot.findFirst({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 
   update(eventId: string, spotId: string, updateSpotDto: UpdateSpotDto) {
-    return `This action updates a #${spotId} spot`;
+    return this.prismaService.spot.update({
+      where: {
+        id: spotId,
+        eventId,
+      },
+      data: updateSpotDto,
+    });
   }
 
   remove(eventId: string, spotId: string) {
-    return `This action removes a #${spotId} spot`;
+    return this.prismaService.spot.delete({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 }
